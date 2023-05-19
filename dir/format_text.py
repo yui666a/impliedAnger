68721dce711fa09de813e751089b0ac8af1b9d44
yui666a <haiso666@gmail.com>
2023-05-19 16:08:52 +0900

    chore: 外部から使用できるように関数型に変更

--- dir/format_text.py
+++ dir/format_text.py
@@ -10,55 +10,75 @@ import nagisa
 import unicodedata
 import re
 
-
+KAOMOJI_LEN = 5
 BASE_PATH = "/home/nb-user/ksl_pub/suggestive_anger/data/fuman_survey"
+SPACY_LOAD_MODEL = "ja_core_news_sm" 
+# SPACY_LOAD_MODEL = "ja_ginza" 
+
+symbol_pattern = r"^[.!?]+"
+leader_pattern = r"[.・]{2,}"
+end_of_sentence_pattern = r"[。!?]$"
+tilde_pattern = "[\u007E\u02DC\u0303\u1FC0\u2053\u223C\u223F\u3030\uFF5E]"
+comma_pattern = r"[､、]{2,}"
+full_stop_pattern = r"[｡。]{2,}"
+unnecessary_full_stop_pattern = r"([!?]+)[｡。]+"
+number_pattern = r"[0-9]+"
+
+compiled_symbol = re.compile(symbol_pattern)
+compiled_leader = re.compile(leader_pattern)
+compiled_eos = re.compile(end_of_sentence_pattern)
+compiled_tilde = re.compile(tilde_pattern)
+compiled_comma = re.compile(comma_pattern)
+compiled_full_stop = re.compile(full_stop_pattern)
+compiled_unnecessary = re.compile(unnecessary_full_stop_pattern)
+compiled_number = re.compile(number_pattern)
 
 # # install packages
-get_ipython().system("pip install emoji nagisa")
-get_ipython().system("pip install -U ginza ja-ginza ja_ginza")
-get_ipython().system("python3 -m spacy download ja_core_news_sm")
+# get_ipython().system("pip install emoji nagisa")
+# get_ipython().system("pip install -U ginza ja-ginza ja_ginza")
+# get_ipython().system("python3 -m spacy download ja_core_news_sm")
 
 # # Load Dataset
 
 
-danwasMap = {
-    "並列": 0,
-    "対比": 1,
-    "要望": 2,
-    "条件": 3,
-    "原因結果": 4,
-    "例提示": 5,
-    "詳細化": 6,
-    "主題連鎖": 7,
-    "焦点主題連鎖": 8,
-    "順接": 9,
-    "逆接": 10,
-    "短文": 11,
-    "質問応答": 12,
-    "理由": 13,
-}
-labelsMap = {0: "感情的攻撃", 1: "感情的説得", 2: "理性的説得", 3: "嫌味皮肉", 4: "遠回し", 5: "怒っていない"}
+# danwasMap = {
+#     "並列": 0,
+#     "対比": 1,
+#     "要望": 2,
+#     "条件": 3,
+#     "原因結果": 4,
+#     "例提示": 5,
+#     "詳細化": 6,
+#     "主題連鎖": 7,
+#     "焦点主題連鎖": 8,
+#     "順接": 9,
+#     "逆接": 10,
+#     "短文": 11,
+#     "質問応答": 12,
+#     "理由": 13,
+# }
+# labelsMap = {0: "感情的攻撃", 1: "感情的説得", 2: "理性的説得", 3: "嫌味皮肉", 4: "遠回し", 5: "怒っていない"}
 
-baseData = pd.read_json(BASE_PATH + "/json/human_data_ver5.json")
-baseData = baseData.rename(columns={"fulltext": "text", "label": "labels"})
-baseData["danwa_result"] = (
-    baseData["danwa_result"].map(danwasMap).fillna(3).astype("int64")
-)
+# baseData = pd.read_json(BASE_PATH + "/json/human_data_ver5.json")
+# baseData = baseData.rename(columns={"fulltext": "text", "label": "labels"})
+# baseData["danwa_result"] = (
+#     baseData["danwa_result"].map(danwasMap).fillna(3).astype("int64")
+# )
 
-kushiroData = pd.concat(
-    [
-        pd.read_csv(BASE_PATH + "/kosen_features.csv"),
-        pd.read_csv(BASE_PATH + "/kosen_labels.csv"),
-    ],
-    axis=1,
-)
-kushiroData = kushiroData.rename(columns={"feature": "text", "label": "labels"})
+# kushiroData = pd.concat(
+#     [
+#         pd.read_csv(BASE_PATH + "/kosen_features.csv"),
+#         pd.read_csv(BASE_PATH + "/kosen_labels.csv"),
+#     ],
+#     axis=1,
+# )
+# kushiroData = kushiroData.rename(columns={"feature": "text", "label": "labels"})
 
-baseData = pd.concat([baseData, kushiroData])
-baseData = baseData.reset_index(drop=True)  # オリジナルのindexを列として保存しない
+# baseData = pd.concat([baseData, kushiroData])
+# baseData = baseData.reset_index(drop=True)  # オリジナルのindexを列として保存しない
 
-print(baseData.labels.value_counts())
-baseData
+# print(baseData.labels.value_counts())
+# baseData
 
 
 # # Load functions
@@ -71,11 +91,6 @@ def extract_emoji(words):
     """
     words = str(words)
     return [w for w in words if emoji.is_emoji(str(w))]
-
-
-# In[9]:
-
-KAOMOJI_LEN = 5
 
 
 def extract_kaomoji(text):
@@ -104,14 +119,8 @@ def extract_kaomoji(text):
     return kaomoji_words
 
 
-# In[10]:
-
-
-# nlp = spacy.load("ja_ginza")
-nlp = spacy.load("ja_core_news_sm")
-
-
 def separate_sentence(feature):
+    nlp = spacy.load(SPACY_LOAD_MODEL)
     doc = nlp(feature)
     return [str(sent) for sent in doc.sents]
 
@@ -120,9 +129,6 @@ def delete(text, target_list):
     for target in target_list:
         text = text.replace(target, "")
     return text
-
-
-# In[11]:
 
 
 def replace_emoji(sentence):
@@ -165,18 +171,6 @@ def delete_kaomoji(sentence):
         print(kaomoji in unicode_sentence)"""
         sentence = sentence.replace(kaomoji, "")
     return sentence
-
-
-tilde_pattern = "[\u007E\u02DC\u0303\u1FC0\u2053\u223C\u223F\u3030\uFF5E]"
-comma_pattern = r"[､、]{2,}"
-full_stop_pattern = r"[｡。]{2,}"
-unnecessary_full_stop_pattern = r"([!?]+)[｡。]+"
-number_pattern = r"[0-9]+"
-compiled_tilde = re.compile(tilde_pattern)
-compiled_comma = re.compile(comma_pattern)
-compiled_full_stop = re.compile(full_stop_pattern)
-compiled_unnecessary = re.compile(unnecessary_full_stop_pattern)
-compiled_number = re.compile(number_pattern)
 
 
 def delete_slash(feature):
@@ -235,14 +229,6 @@ def fix_bracket(sentences, left_bracket, right_bracket):
             should_skip = True
 
     return new_sentences
-
-
-symbol_pattern = r"^[.!?]+"
-leader_pattern = r"[.・]{2,}"
-end_of_sentence_pattern = r"[。!?]$"
-compiled_symbol = re.compile(symbol_pattern)
-compiled_leader = re.compile(leader_pattern)
-compiled_eos = re.compile(end_of_sentence_pattern)
 
 
 def join_symbol_only_sentence(sentences):
@@ -331,19 +317,17 @@ def make_sentences_from_feature(feature):
     )
 
 
-# In[13]:
-
-
-features = []
-
-for i, data in enumerate(baseData.values):
-    feature = data[0]
-    feature = unicodedata.normalize("NFKC", feature)  # NFKC正規化
-    # emoji_list = extract_emoji(feature)
-    result = make_sentences_from_feature(feature)
-    line_length = len(result)
-    sentence = " ".join(result)
-    features.append([i, line_length, sentence])  # [index, 文章数, 本文]
-    # for sentence_i, sentence in enumerate(result):
-    #     # index, 何文章目か, 文章数, 本文
-    #     features.append([i, sentence_i, len_result, sentence])
+def format_df_text(df):
+    features = []
+    for i, data in df.iterrows():
+        print(data["text"])
+        feature = data["text"]
+        feature = unicodedata.normalize('NFKC', feature) # NFKC正規化
+        # emoji_list = extract_emoji(feature)
+        result = make_sentences_from_feature(feature)
+        line_length = len(result)
+        sentence = ' '.join(result)
+        features.append([i, line_length, sentence]) # [index, 文章数, 本文]
+        # for sentence_i, sentence in enumerate(result):
+        #     # index, 何文章目か, 文章数, 本文
+        #     features.append([i, sentence_i, len_result, sentence]) 
